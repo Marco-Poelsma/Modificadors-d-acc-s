a77d879bff0c973f3c034e5bb046d1bd6efd8fe9
Marco Poelsma <marco.poelsma@gracia.lasalle.cat>
2025-01-24 09:36:51 +0100

    Keywords protected i private

--- alumne/Alumne.java
+++ alumne/Alumne.java
@@ -1,9 +1,9 @@
 package alumne;
 
-public class Alumne {
+public class Alumne { //El public fa que sigui visible des de qualsevol altra classe, independentment del paquet on es trobi
     String name; //Si no té modificador, només s'hi pot acedir des del paquet on es troba
-    String username;
-    String password;
+    protected String username; //Només s'hi pot accedir des del paquet on es troba i des de les seves subclasses
+    private String password; //Només s'hi pot accedir des de la mateixa classe
 
     String generateUsername (String name) {
         return name.strip().toLowerCase().replace(" ", "");
